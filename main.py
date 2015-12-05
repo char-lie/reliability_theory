737d7fd88c06f8628d6b3a61b58f1841a1c399f2
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-05 19:06:28 +0200

    Increased performance

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ from math import exp, log
 from numpy.random import exponential
 from scipy.stats import poisson
 from numpy import dot
-from decimal import Decimal, getcontext
-
-#getcontext().prec = 100
+from decimal import Decimal
+from joblib import Parallel, delayed
 
 def F(x, rho):
     return 1 -  exp(-rho * x)
 
 def get_f(rho):
-    return lambda x: rho * Decimal(-rho * x).exp()
+    return lambda x: rho * exp(-rho * x)
 
 def G(x):
-    return 1 - Decimal(-x).exp()
+    return 1 - exp(-x)
 
 def get_t(rho, n):
-    return map(Decimal, exponential(1/rho, n))
+    return exponential(1./rho, n)
 
 def get_p(t, a, n):
-    accumulator = Decimal(0)
+    accumulator = 0
     result = []
     for i in xrange(n):
         accumulator += t[i] * a[i]
@@ -52,13 +51,9 @@ def get_R(n, p, r):
         result += s(n, k)
     return result
 
-def get_Q(R, f, a, t):
-    dividends = [A * f(A*T) for A, T in zip(a,t)]
-    divisors = [f(T) for T in t]
-    mul = lambda x, y: x*y
-    di = reduce(mul, dividends, 1)
-    d = reduce(mul, divisors, 1)
-    return Decimal(R) * di / d
+def get_Q(R, f, a, t, D=float):
+    d = [A * f(A*T) / f(T) for A, T in zip(a,t)]
+    return D(R) * reduce(lambda x,y: D(x)*D(y), d, D(1))
 
 def get_n(epsilon, rho, a, t):
     w = poisson.isf(epsilon, 1.0/rho)
@@ -70,37 +65,61 @@ def get_n(epsilon, rho, a, t):
     return n
 
 def get_a(phi, n):
-    return [Decimal(phi(i)) for i in xrange(1, n+1)]
+    return [phi(i) for i in xrange(1, n+1)]
 
 def get_phi(m, A):
     b = (exp(1) - exp(A)) / (m - 1)
     a = exp(A) - b
     return (lambda x: log(a + b * x))
 
-if __name__ == '__main__':
-    r = 100
-    rho = Decimal(150)
-    n = 400
-    m = 300
-    epsilon = 1E-4
-    A = 0.9
+def calculate_Q(r, rho):
+    k = 1
+    c = Decimal(-rho).exp()
+    result = 0
+    for i in xrange(1, r):
+        k *= i
+        result += c * (rho**i)/k
+    return 1 - c - result
 
-    phi = get_phi(m, A)
-    print 'Init a'
-    a = get_a(phi, n)
-    #print a
-    print 'Init t'
+def run_task(rho, r, n, a, f, D=float):
     t = get_t(rho, n)
-    #print t
-    print 'Init p'
     p = get_p(t, a, n)
-    #print p
-    print 'Init f'
-    f = get_f(rho)
 
-    print 'Init R'
     R = get_R(n, p, r)
-    print 'Init Q'
-    Q = get_Q(R, f, a, t)
-    print 'Result is', Q
+    Q = get_Q(R, f, a, t, D)
+    return Q
+
+def init_a(n, m, A):
+    phi = get_phi(m, A)
+    a = get_a(phi, n)
+    return a
+
+def stdev(real, experimental):
+    return sum((Decimal(e) - real)**2 for e in experimental)/len(experimental)
+
+if __name__ == '__main__':
+    iterations = 100
+    r = 100
+    rho = 150
+    n = 300
+    m = 2*r
+    epsilon = 1E-4
+    D = Decimal
+    A = 1 - D('1E-10')
+    realQ = D(calculate_Q(r, rho))
+
+    Qs = []
+    a = init_a(n, m, A)
+    f = get_f(rho)
+    def get_task(i):
+        print 'Running task', i
+        return run_task(rho, r, n, a, f, D)
+    #Qs = Parallel(n_jobs=4)(delayed(get_task)(i) for i in range(iterations))
+
+    for i in range(iterations):
+        print 'Running task', i
+        Qs.append(run_task(rho, r, n, a, f, D))
+    experimentalQ = sum(Qs)/len(Qs)
+    print 'Real is %f, experimental is %f, difference is %f%%, dev is %f'%(float(realQ), float(experimentalQ),
+           100.0*abs(float((realQ-experimentalQ)/realQ)), float(stdev(realQ, Qs)))
 
