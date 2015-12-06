21eb3144dfed5c466dd57ef413bcff0bba9c7ba8
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-06 20:50:05 +0200

    Added optimal values

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def run_task(rho, r, n, a, f, t):
 
 def stdev(real, experimental):
     avg = sum(experimental)/len(experimental)
-    return sum((e - avg)**2 for e in experimental)/(sum(experimental))
+    return float(sum(Decimal(e - avg)**2 for e in experimental)/(Decimal(sum(experimental))))
 
 def run_tasks(rho, r, A, m, iterations=1, parallel=False, D=float, get_task=None):
     realQ = D(calculate_Q(r, rho))
@@ -107,7 +107,7 @@ def run_tasks(rho, r, A, m, iterations=1, parallel=False, D=float, get_task=None
     Qs = []
     f = get_f(rho)
     if parallel:
-        Qs = Parallel(n_jobs=4)(delayed(get_task)(A, m, i) for i in range(iterations))
+        Qs = Parallel(n_jobs=4)(delayed(get_task)(A, m, r, i) for i in range(iterations))
     else:
         for i in range(iterations):
             #profile.enable()
@@ -123,44 +123,61 @@ def run_tasks(rho, r, A, m, iterations=1, parallel=False, D=float, get_task=None
 
 
 if __name__ == '__main__':
-    iterations = 1
-    r = 140
+    iterations = 512
+    r = 160
     rho = 100
     m = 2*r
     #epsilon = 1E-5
-    epsilon = 1E-5
+    epsilon = 1E-4
+    RMA = [(140,280,.73),(160,320,.635),(180,360,.55)]
     #A = 0.9
     # r = 120, A = .93, m = 200
     # r = 140
-    # Difference is 7.873009%, result is 0.000084, deviation is 0.000038 (a*=0.750000, m=280, r=140)
-    # Difference is 1.466868%, result is 0.000090, deviation is 0.000112 (a*=0.800000, m=280, r=140)
-    # Difference is 14.040876%, result is 0.000079, deviation is 0.000335 (a*=0.850000, m=280, r=140)
+    # Difference is 6.797045830938964%, result is 0.000097875769842, deviation is 0.000023861530866 (a*=0.720000, m=280, r=140)
+    # Difference is 5.523799854110090%, result is 0.000086584144244, deviation is 0.000023247527594 (a*=0.730000, m=280, r=140)
+    # Difference is 1.289890786034022%, result is 0.000090464374322, deviation is 0.000023518769262 (a*=0.740000, m=280, r=140)
+    # Difference is 2.246506493257931%, result is 0.000089587669371, deviation is 0.000033948674798 (a*=0.750000, m=280, r=140)
     # r = 160
     # Difference is 5.016462846447344%, result is 0.000000021535700, deviation is 0.000000007443211 (a*=0.620000, m=320, r=160)
+    # Difference is 3.234725141990860%, result is 0.000000021170320, deviation is 0.000000005834300 (a*=0.625000, m=320, r=160)
     # Difference is 5.586814696371359%, result is 0.000000021652662, deviation is 0.000000005401941 (a*=0.630000, m=320, r=160)
-    # Difference is 1.521000899725865%, result is 0.000000020818887, deviation is 0.000000011034985 (a*=0.640000, m=320, r=160)
+    # Difference is 6.719558156155639%, result is 0.000000019128997, deviation is 0.000000004220299 (a*=0.635000, m=320, r=160)
+    # Difference is 8.664290838406769%, result is 0.000000018730191, deviation is 0.000000004405956 (a*=0.640000, m=320, r=160)
+    # Difference is 3.447507324567010%, result is 0.000000021213955, deviation is 0.000000007668876 (a*=0.645000, m=320, r=160)
     # r = 180
     # Difference is 0.790672701320627%, result is 0.000000000000407, deviation is 0.000000000000380 (a*=0.500000, m=360, r=180)
+    # Difference is 1.998976948925200%, result is 0.000000000000402, deviation is 0.000000000000177 (a*=0.520000, m=360, r=180)
+    # Difference is 6.971219955756800%, result is 0.000000000000382, deviation is 0.000000000000121 (a*=0.540000, m=360, r=180)
     # Difference is 1.419735970524463%, result is 0.000000000000405, deviation is 0.000000000000148 (a*=0.550000, m=360, r=180)
+    # Difference is 2.642272672916865%, result is 0.000000000000421, deviation is 0.000000000000126 (a*=0.560000, m=360, r=180)
+    # Difference is 7.584047091959102%, result is 0.000000000000379, deviation is 0.000000000000166 (a*=0.580000, m=360, r=180)
     # Difference is 15.120959474991036%, result is 0.000000000000348, deviation is 0.000000000000274 (a*=0.600000, m=360, r=180)
 
-    realQ = float(calculate_Q(r, rho))
+    #realQ = float(calculate_Q(r, rho))
     f = get_f(rho)
-    def get_task(A, m, i):
+    def get_task(A, m, r, i):
         seed()
         n, a, t = generate_n(epsilon, rho, m, A)
         Q = run_task(rho, r, n, a, f, t)
+        realQ = float(calculate_Q(r, rho))
         print 'Compeleted task %04d for a*=%f, m=%d with n=%04d: %.15f (%.15f, %f%%)'%(i, A, m, n, Q, realQ, 100*abs(realQ-Q)/realQ)
+        stdout.flush()
         return Q
 
     results = []
-    #for i in xrange(5):
-    #    results.append(run_tasks(rho, r, .52+.02*i, m, iterations, True, get_task=get_task))
-    results.append(run_tasks(rho, r, .93, m, iterations, True, get_task=get_task))
-    realQ = float(calculate_Q(r, rho))
+    #for i in xrange(2):
+    #    results.append(run_tasks(rho, r, .64+.005*i, m, iterations, True, get_task=get_task))
+    #Qs = results[0][5]
+    for r, m, a in RMA:
+        results.append(run_tasks(rho, r, a, m, iterations, True, get_task=get_task))
+    #realQ = float(calculate_Q(r, rho))
     #devs = [stdev(realQ, Qs[:i]) for i in xrange(1,len(Qs)+1)]
     #print devs
-    for result in results:
-        print 'Difference is %.15f%%, result is %.15f, deviation is %.15f (a*=%f, m=%d, r=%d)'%(100*abs(realQ-result[0])/realQ, result[0], result[1], result[2], result[3], result[4])
+    for experimentalQ, stdevi, A, m, r, Qs in results:
+        realQ = float(calculate_Q(r, rho))
+        print 'Difference is %.15f%%, result is %.15f (%.15f), deviation is %.15f (a*=%f, m=%d, r=%d)'%(100*abs(realQ-experimentalQ)/realQ, experimentalQ, realQ, stdevi, a, m, r)
     #Stats(profile).sort_stats('tottime').print_stats()
+    for experimentalQ, stdevi, A, m, r, Qs in results:
+        print 'r =', r
+        print Qs
 
