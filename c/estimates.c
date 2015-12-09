2b4123edc56f79ddbff8cf4333ecf8661838aa3c
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-09 18:11:12 +0200

    Fixed type of function s

--- c/estimates.c
+++ c/estimates.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "parameters.h"
 
-double* s(size_t n, double* p) {
+double** s(size_t n, double* p) {
     double** matrix = (double**)malloc(n * sizeof(double*));
     matrix[0] = (double*)malloc(sizeof(double));
     matrix[0][0] = 1;
@@ -19,7 +19,7 @@ double* s(size_t n, double* p) {
 }
 
 double R(size_t n, size_t r, double* p) {
-    double* s_matrix = s(n, p);
+    double** s_matrix = s(n, p);
     double result = 0;
     do {
         result += s_matrix[n][r];
--- c/estimates.h
+++ c/estimates.h
@@ -1,7 +1,7 @@
 #ifndef ESTIMATES
 #define ESTIMATES
 
-double* s(size_t n, double* p);
+double** s(size_t n, double* p);
 double R(size_t n, size_t r, double* p);
 double Q(double R, double rho, double* a, double* t);
 
