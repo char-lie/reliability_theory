e837e7564c5a039aeb8b90e04117b05e3859e76b
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-09 16:48:43 +0200

    Changed signature of get_a and estimate_n: n and m are first parameters

--- c/parameters.c
+++ c/parameters.c
@@ -8,7 +8,7 @@ double* get_t(size_t n, double rho) {
     return result;
 }
 
-double* get_a(size_t m, double alpha, size_t n) {
+double* get_a(size_t n, size_t m, double alpha, ) {
     double a = (m * exp(A) - exp(1)) / (m - 1);
     double b = (exp(1) - exp(alpha)) / (m - 1);
     double* result = (double*)malloc(n * sizeof(double))
@@ -22,7 +22,7 @@ double* get_a(size_t m, double alpha, size_t n) {
     return result;
 }
 
-size_t estimate_n(int rho, double epsilon, double alpha, size_t m) {
+size_t estimate_n(size_t m, int rho, double epsilon, double alpha) {
     return m + poisson_quantile(epsilon) * rho - (1 + alpha) * m / 2;
 }
 
--- c/parameters.h
+++ c/parameters.h
@@ -1,4 +1,4 @@
-size_t estimate_n(int rho, double epsilon, double alpha, size_t m);
-double* get_a(size_t m, double alpha, size_t n);
+size_t estimate_n(size_t m, int rho, double epsilon, double alpha);
+double* get_a(size_t n, size_t m, double alpha);
 double* get_t(size_t n, double rho);
 
