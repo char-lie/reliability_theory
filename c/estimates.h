ca32e3a110190e1fcfa2a63d2e8399c234491ee6
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-09 17:49:02 +0200

    Added difines to prevent headers information reinclude

--- c/estimates.h
+++ c/estimates.h
@@ -1,4 +1,9 @@
+#ifndef ESTIMATES
+#define ESTIMATES
+
 double* s(size_t n, double* p);
 double R(size_t n, size_t r, double* p);
 double Q(double R, int rho, double* a, double* t);
+
+#endif
 
--- c/randoms.h
+++ c/randoms.h
@@ -1,3 +1,8 @@
+#ifndef RANDOMS
+#define RANDOMS
+
 double random_exponential(int rho);
 double poisson_quantile(double epsilon);
+
+#endif
 
