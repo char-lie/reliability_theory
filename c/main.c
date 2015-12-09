8afc14c9a2722354d1512239a959bd92aeae3cbd
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-09 19:25:25 +0200

    Made main iterative

--- c/main.c
+++ c/main.c
@@ -6,6 +6,8 @@
 int main() {
     srand(time(NULL));
 
+    size_t iterations = 10000;
+
     double rho = 100.0;
     double epsilon = 1E-4;
 
@@ -13,20 +15,25 @@ int main() {
 
     size_t m = r * 2;
     double alpha = 71.6E-2;
+    long double avgQ = 0.0;
 
-    size_t n = estimate_n(m, rho, epsilon, alpha);
-    double* a = get_a(n, m, alpha);
-    double* t = get_t(n, rho);
-    double* p = get_p(n, a, t);
+    size_t i=iterations;
+    while (--i > 0) {
+        size_t n = estimate_n(m, rho, epsilon, alpha);
+        double* a = get_a(n, m, alpha);
+        double* t = get_t(n, rho);
+        double* p = get_p(n, a, t);
 
-    double R_estimate = R(n, r, p);
-    double Q_estimate = Q(R_estimate, rho, n, a, t);
+        double R_estimate = R(n, r, p);
+        double Q_estimate = Q(R_estimate, rho, n, a, t);
 
-    printf("Q estimate is %f, real value is %f\n", Q_estimate, get_Q(r, rho));
+        avgQ += Q_estimate;
 
-    free(a);
-    free(t);
-    free(p);
+        free(a);
+        free(t);
+        free(p);
+    }
+    printf("Avg=%.015Lf, real is %.015f\n", avgQ/iterations, get_Q(r, rho));
     return 0;
 }
 
