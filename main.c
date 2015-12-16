4c48f0614d16d594d2a5157b752f2127e189e2d7
char-lie <5962274+char-lie@users.noreply.github.com>
2015-12-16 21:53:39 +0200

    Refactored command-line parameters

--- main.c
+++ main.c
@@ -9,27 +9,45 @@
 #include "experiment/experiment.h"
 #include "experiment/alpha.h"
 
-enum EstimateAction {ESTIMATE_PARAMETERS, ESTIMATE_PROBABILITY};
+enum EstimateAction {
+    ESTIMATE_PARAMETERS, ESTIMATE_PROBABILITY,
+    CALCULATE_PROBABILITY
+};
 
 int print_usage (char* executable) {
-    printf("USAGE: %s [action] options...\n\n", executable);
+    printf("\nUSAGE: %s [action] options...\n\n", executable);
     printf(
-        "  action\n                 Action to perform\n"
-        "          -p, --parameters You want to estimate parameters\n"
-        "                               a* and m for specified r option\n"
-        "          -e, --estimate   You want to get estimate for Q\n"
-        "                               and compare it with real Q\n\n"
+        "action\n"
+        "  -p, --parameters  Estimate parameters"
+                           " a* and m for specified r option\n"
+        "  -Q, --estimate    Estimate Q and compare it with real Q\n"
+        "  -P, --probability Calculate real values for Q\n\n"
     );
     printf(
-        "  options\n                 Positional parameters\n"
-        "           r  [unsigned]    The only option for a* and m estimate\n"
-        "           m  [unsigned]    Mandatory for Q estimate\n"
-        "           a* [float]       Mandatory for Q estimate\n\n"
+        "options: (m, a*) parameters estimate\n"
+        "  r     [unsigned] Parameter for Q probability: Q(r) = P{nu >= r},\n"
+        "                   where nu is an amount of entities being observed\n"
+        "  N     [unsigned] Estimates amount for each (m,a*) tuple\n\n"
     );
     printf(
-        "  examples\n"
-        "%s -p 140                   Get a* and m for r=140\n"
-        "%s --estimate 140 270 0.75  Estimate Q for r=140, m=170, a*=0.75\n\n",
+        "options: Q probability estimate\n"
+        "  r     [unsigned] Parameter for Q probability: Q(r) = P{nu >= r},\n"
+        "                   where nu is an amount of entities being observed\n"
+        "  m     [unsigned] Vector `alpha` scale\n"
+        "  a*    [float]    Vector `alpha` minimal value\n\n"
+        "  E     [float]    Epsilon, occuracy\n\n"
+    );
+    printf(
+        "options: real Q values\n"
+        "  r...  [unsigned] Parameters for Q probability: Q(r) = P{nu >= r},\n"
+        "                   where nu is an amount of entities being observed\n"
+    );
+    printf(
+        "examples\n"
+        "Get a* and m for r=140:\n"
+        "%s -p 140\n"
+        "Estimate Q for r=140, m=170, a*=0.75\n"
+        "%s --estimate 140 270 0.75\n\n",
     executable, executable);
     return 0;
 }
@@ -41,27 +59,28 @@ int main (int argc, char** argv) {
     if (argc > 1) {
         if (!strcmp(argv[1], "--parameters") || !strcmp(argv[1], "-p")) {
             action = ESTIMATE_PARAMETERS;
-        } else if (!strcmp(argv[1], "--estimate") || !strcmp(argv[1], "-e")) {
+        } else if (!strcmp(argv[1], "--estimate") || !strcmp(argv[1], "-Q")) {
             action = ESTIMATE_PROBABILITY;
+        } else if (!strcmp(argv[1], "--probability") || !strcmp(argv[1], "-P")) {
+            action = CALCULATE_PROBABILITY;
         } else if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
             print_usage(*argv);
             return 0;
         }
     }
-    if (action == ESTIMATE_PROBABILITY && argc < 5) {
+    if ((action == ESTIMATE_PROBABILITY && argc < 5) || argc < 3) {
         print_usage(*argv);
         return 1;
     }
 
     srand(time(NULL));
 
-
     struct EstimateParameters params = {
         .rho = 100.0,
-        .epsilon = 1E-4,
-        .r = argc > 2 ? (size_t)atoi(argv[2]) : 180,
+        .r = (size_t)atoi(argv[2]),
         .m = argc > 3 ? (size_t)atoi(argv[3]) : 0,
         .alpha = argc > 4 ? (float)atof(argv[4]) : 0.0,
+        .epsilon = argc > 5 ? (float)atof(argv[5]) : 1E-4,
     };
 
     size_t iterations;
@@ -99,6 +118,17 @@ int main (int argc, char** argv) {
                     fabs((sum(values, iterations)/iterations-realQ)/realQ),
                     sum(values, iterations)/iterations, realQ);
             free(values);
+            break;
+        case CALCULATE_PROBABILITY:
+            i = 1;
+            while (++i < argc) {
+                params.r = (size_t)atoi(argv[i]);
+                printf("%zu: %E\n", params.r, get_Q(params.r, params.rho));
+            }
+            break;
+        default:
+            fprintf(stderr, "Unknown option\n");
+            return 1;
             break;
     }
 
